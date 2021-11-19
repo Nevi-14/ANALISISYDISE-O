b17da03777a5c4d22eed0275c9a989c6082b9ff7
Nelson Morera <workemailnelson@gmail.com>
2021-11-19 16:01:19 -0600

    2021119v1.1

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+var displayButton = document.getElementById('display');
+var wrapper = document.getElementById('wrapper');
+displayButton.addEventListener('click', function(){
+wrapper.classList.toggle('menuDisplayed')
+});
